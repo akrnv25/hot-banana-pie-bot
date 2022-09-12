4000442c34f9a5b063ebd6e7a2dcb38b580f7583
akrnv25 <akrnv25@gmail.com>
2022-09-12 10:34:45 +0300

    added root page

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:16.13.0
+FROM node:16.13.0-alpine3.14
 WORKDIR /hot-banana-pie-bot
 COPY ./package.json ./
 RUN npm install
--- src/routers/api.js
+++ src/routers/api.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const sendMessageController = require('../controllers/api/send-message');
+const rootController = require('../controllers/api/root');
 
 const router = express.Router();
 
+router.get('', rootController);
 router.post('/send-message', sendMessageController);
 
 module.exports = router;
